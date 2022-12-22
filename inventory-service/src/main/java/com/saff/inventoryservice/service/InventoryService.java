1a2a2e19d7e1614b5454236beba45b26fd58ae07
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2022-12-21 19:09:32 -0500

    add Zipkin

--- inventory-service/src/main/java/com/saff/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/saff/inventoryservice/service/InventoryService.java
@@ -20,9 +20,6 @@ public class InventoryService {
     @Transactional(readOnly = true)
     @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCodes) {
-        log.info("Wait started");
-        Thread.sleep(10000);
-        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCodes)
                 .stream()
                 .map(inventory ->
