0e1f88297a8609dae4fb3b669842b68800a26e7b
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2022-12-23 11:51:53 -0500

    add Notification service
    Add Kafka producer and consumer

--- order-service/src/main/java/com/saff/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/saff/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.saff.orderservice.service;
 import com.saff.orderservice.dto.InventoryResponse;
 import com.saff.orderservice.dto.OrderLineItemsDto;
 import com.saff.orderservice.dto.OrderRequest;
+import com.saff.orderservice.event.OrderPlacedEvent;
 import com.saff.orderservice.model.Order;
 import com.saff.orderservice.model.OrderLineItems;
 import com.saff.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,7 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -53,6 +55,7 @@ public class OrderService {
 
         if(allProductsInStock) {
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order placed successfully";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again!");
